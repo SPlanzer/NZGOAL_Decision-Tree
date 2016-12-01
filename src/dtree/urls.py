a4d77f4dc2ce6f68eb1958a0a79ebb265e0b7ab2
Splanzer <splanzer@linz.govt.nz>
2016-12-01 13:07:18 +1300

    removed unnecessary imports

--- src/dtree/urls.py
+++ src/dtree/urls.py
@@ -1,13 +1,13 @@
 from django.conf import settings
 from django.conf.urls import url, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
 from django.conf.urls.static import static
-from django.conf.global_settings import STATICFILES_DIRS
 from . import views
 
 app_name = 'dtree'
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^about/$', views.about, name = 'about'),
     url(r'datasets/$|^$', views.datasets, name = 'datasets'),
     url(r'^(?P<dataSet_id>[0-9]+)/$', views.detail, name = 'detail'),
@@ -19,16 +19,7 @@ urlpatterns = [
     url(r'no/$', views.uDecision, name='no'),
     url(r'ok/$', views.uDecision, name='ok'),
     
-    #surl(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATICFILES_DIRS}),
 ]
-# #ths is to ensure we dont go live with this statis set up
-#if settings.DEBUG:
-#urlpatterns += staticfiles_urlpatterns()
 
-# if settings.DEBUG:
-#     urlpatterns += patterns('',
-#             (r'^static/(.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_PATH}),
-#     )
-#     
-
-    
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
